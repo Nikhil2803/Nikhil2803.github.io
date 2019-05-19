3ab1aca299bb6ecdfa8af9810ebe3af7bc7b2745
Nikhil2803 <nikhil.k320@gmail.com>
2019-05-19 13:54:55 +0530

    Tax Caculator V-0.0.1
    Tax Caculator For Indian Salaried Tax payer

---
+++ taxcalculator/tax_slabs_jsons.js
@@ -0,0 +1,508 @@
+var taxSlabs202021 = 
+	{
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+var taxSlabs201920 = 
+	{
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":4,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+	
+var taxSlabs201819 = 
+	{
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":5,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"5000000":10,"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+	
+var taxSlabs201718 = 
+	{
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":15},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+	
+var taxSlabs201617 = 
+	{
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+	
+var taxSlabs201516 = 
+	{
+		
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+	
+var taxSlabs201415 = 
+	{
+		
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+var taxSlabs201314 = 
+	{
+		
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+var taxSlabs201213 = 
+	{
+		
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+var taxSlabs201112 = 
+	{
+		
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+	
+	
+var taxSlabs201011 = 
+	{
+		
+		"Men":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"1000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"Women":{
+		"incomeTaxRate":{"250000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SeniorCitizen":{
+		"incomeTaxRate":{"300000":0,"500000":10,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		},
+		"SuperSeniorCitizen":{
+		"incomeTaxRate":{"500000":0,"1000000":20,"Max":30 },
+		"surcharge":{"10000000":12},
+		"EducationCess":3,
+		"limit80C":150000,
+		"limit80E":150000,
+		"limit80T":10000,
+		"limit80DMax":100000,
+		"taxRelief87A":2500
+		}
+	}
+
+	
+	
+	
